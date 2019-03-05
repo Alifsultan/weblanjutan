f0aa28622567c347ffd6954657bf8c216e2f5e77
Alifsultan <alifsultan22@gmail.com>
2019-03-05 22:48:19 +0700

    Update courses.component.ts

--- Praktikum4/src/app/courses/courses.component.ts
+++ Praktikum4/src/app/courses/courses.component.ts
@@ -24,7 +24,7 @@ export class CoursesComponent implements OnInit {
   imageUrl = 'http://lorempixel.com/400/200';
   colSpan = 2;
   isActive = true;
-  nama = 'Izmir';
+  nama = 'ALIF';
 
   getTitle() {
     return this.title;
